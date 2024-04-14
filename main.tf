2654c50f56d4e5d4476d0187999290f49c22610c
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-14 19:33:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,13 +56,13 @@ module "blog_alb" {
     }
   }
 
-  http_tcp_listeners = {
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
