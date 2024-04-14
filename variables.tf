8fdafc371b7c5ce6f7a6418a5a36d32cf531eb45
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-14 09:54:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "secretkey" {
+    type = string
+  }
+
+  variable "accesskey" {
+    type = string
+    
+  }
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
