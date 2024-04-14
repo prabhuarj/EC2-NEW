8fd2fa3eab089d76bd8ac8b3911916a700e3f859
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-14 19:40:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "vm" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "prabhuvm"
   }
 }
 
