83562a719f0df695488a74aac0f054c1a9feaaa1
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-14 10:02:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "secretkey" {
     
   }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
-}
+  variable "instance_type" {
+    description = "Type of EC2 instance to provision"
+    default     = "t2.micro"
+  }
