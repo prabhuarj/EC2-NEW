e7febcb4554d92d00a657fc3f06454397aef3c7e
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-15 11:56:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,28 +50,27 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups    = [module.sg.security_group_id]
 
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      }
+    }
+
   target_groups = {
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
     }
   }
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
   tags = {
-    Environment = "dev"
+    Environment = "Dev"
+   
   }
 }
-
 
 
 module "sg" {
