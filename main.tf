07165962063d49248923ce42819c06785c79d28d
prabhuarj <162825059+prabhuarj@users.noreply.github.com>
2024-04-16 11:39:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,15 +30,19 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "vm" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
 
-  tags = {
-    Name = "prabhuvm"
-  }
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "alb" {
